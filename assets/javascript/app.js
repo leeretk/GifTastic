d745dcdd4329469bc12026e3afd4ed9c1688d6a7
Kamy <kamy.leeret@uchealth.org>
2020-03-07 21:18:59 -0700

    its broken for the moment.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,51 +3,53 @@ var categories = [
     "icehockey", "toedrag", "stickhandling", "puckhandling", "amazingplays", "gretksy",
 ];
 
-var categoriesLength = categories.length;
+//giffs = $(this).attr("category-name");
 
-function queryApis() {
+function queryGiffs() {
+    
+    giff = $("#giff-input").val().trim();
 
-    category = $("#category-input").val().trim();
-    categories = $(this).attr("category-name");
-
-    var queryOMDB = "https://www.omdbapi.com/?t=" + category + "&apikey=trilogy";
-
-    var queryGIPHY = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
-
-    $.ajax({
-        url: queryOMDB,
-        // data: {
-        //     title, 
-        //     poster,
-        //     rating,
-        //     },
-        method: "GET"
-    }).then(function (response) {
-        console.log("Title: " + response.title);
-        console.log("Runtime: " + response.Runtime);
-        console.log("Poster: " + response.poster);
-        console.log("Rating: " + response.rating);
-        $("#omdb-view").text(JSON.stringify(response));
-    });
+    var queryGIPHY = "https://api.giphy.com/v1/gifs/search?q=" + giff + "@api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
 
     $.ajax({
         url: queryGIPHY,
         method: "GET"
     }).then(function (response) {
-        console.log(response.data);
+        console.log(response);
+        console.log(response);
         $("#giphy-view").text(JSON.stringify(response))
     });
 };
 
-$("#find-content").on("click", function (event) {
-            event.preventDefault();
-            
-            queryApis();
-            console.log(categories);
-});
- 
- 
- 
+movie = $("movie-input").val().trim();
+//movies = $(this).attr("movie-name");
+
+function queryMovies() {
+    var queryOMDB = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy&limit=5";
+
+    $.ajax({
+        url: queryOMDB,
+        method: "GET"
+    }).then(function (response) {
+        console.log("Title: " + response.Title);
+        console.log("Runtime: " + response.Runtime);
+        console.log("Poster: " + response.Poster);
+        console.log("Rated: " + response.Rated);
+        $("#omdb-title").text(JSON.stringify("Title: " + response.Title));
+        $("#omdb-runtime").text(JSON.stringify("Runtime: " + response.Runtime));
+        $("#omdb-poster").text(JSON.stringify("Poster: " + response.Poster));
+        $("#omdb-rating").text(JSON.stringify("Rated: " + response.Rated));
+    });
+
+    ///this connects to the button 
+    $("#find-content").on("click", function (event) {
+        event.preventDefault();
+        queryMovies();
+        queryGiffs();
+    });
+
+ //
+
         //renderButtons();
     //find-content
     //category-form
@@ -78,4 +80,3 @@ $("#find-content").on("click", function (event) {
     //     $(document).on("click", ".category", displaycontent);
 
     //     renderButtons();       
-
